26de42353bd405d74c83fc8238f3dd22eca0403e
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-03-07 21:22:08 +0100

    removed

--- payment/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def payment_successs(request):
-    return render(request, 'payment/payment-success.html')
-
-def payment_failed(request):
-    return render(request, 'payment/payment-failed.html')
